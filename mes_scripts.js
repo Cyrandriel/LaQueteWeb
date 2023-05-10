928e91ccb3073db26c8c82df4c8ab5db99f191c6
jukyob <jukyob@gmail.com>
2023-05-10 13:06:25 +0200

    prin

---
+++ mes_scripts.js
@@ -0,0 +1,20 @@
+function demarrage() {
+    document.getElementById("mon_titre").innerHTML = "J'ai modifié le titre";
+    document.getElementById("premier_paragraphe").innerHTML = "5*3="+(5*3);
+}
+
+
+function togg(){
+    var div = document.getElementById("d1");
+    if(div.style.display != "none"){
+      div.style.display = "none";
+    } else {
+      div.style.display = "block";
+    }
+};
+
+let x = 0;
+function add(){
+    x = x+1;
+    alert(x);
+}
